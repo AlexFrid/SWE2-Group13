7c8aefff102fbd9467784907b73461c1494fd9b9
nat-bialek <93878157+nat-bialek@users.noreply.github.com>
2021-11-28 21:24:50 +0000

    Separated registration from login
    This commit adds some refactoring for easier reading + enables users to log in after registration (registration no longer automatically logs in the user into their account)

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -21,43 +21,14 @@ public class NewBankClientHandler extends Thread{
 	
 	public void run() {	
 		try {
-			// 1. Process intial request (login or register)
-			out.println(
-			"####################\n" +
-			"Welcome to New Bank\n" +
-			"####################");
-			out.println("Type LOGIN if you are an existing user, or REGISTER if you are a new user");
-			String intialCommand = in.readLine();
-			boolean requestValid = false;
-			while(!requestValid) {
-				if(intialCommand.equals("LOGIN") || intialCommand.equals("REGISTER")) {
-					requestValid = true;
-				}
-				else {
-					out.println("Command not recognised. Please enter command LOGIN or REGISTER");
-					intialCommand = in.readLine();
-				}
-			}
+			// 1. Process initial request (login or register)
+			String intialCommand = greetUser();
 
 			// 2. Control flow for request 
 			CustomerID customer = null;
 			while(true) {
 				if(intialCommand.equals("LOGIN")) {
-					// ask for user name
-					out.println("Enter Username");
-					String userName = in.readLine();
-					// ask for password
-					out.println("Enter Password");
-					String password = in.readLine();
-					out.println("Checking Details...");
-					customer = bank.checkLogInDetails(userName, password);
-					//Validate login details
-					if(customer == null) {
-						out.println("Log In Failed. Invalid Credentials, please try again.");
-					}
-					else {
-						out.println("Log In Successful. What do you want to do?");
-					}
+					customer = login();
 				} 
 				else {
 					//ask for user name
@@ -69,9 +40,13 @@ public class NewBankClientHandler extends Thread{
 					out.println("Checking Details...");
 					//Validate account details
 					if(bank.isPasswordValid(password) && bank.isUserNameValid(userName)) {
-						customer = bank.registerCustomer(userName, password);
-						out.println("Registration successful. What do you want to do?");
-					} 
+						bank.registerCustomer(userName, password);
+						out.println("Registration successful. You can now log in");
+						while(customer == null) {
+							customer = login();
+						}
+					}
+					
 					else {
 						customer = null;
 						out.println("Registration failed. The username and password entered must be valid, please try again");
@@ -83,11 +58,13 @@ public class NewBankClientHandler extends Thread{
 					out.println("SHOWMYACCOUNTS - view your accounts and their balances");
 					out.println("RESETPASSWORD <Password> - reset login password");
 					out.println("CREATEACCOUNT <Account name> <Opening balance> - create new bank account");
+					out.println("MOVE <Amount> <Account name 1> <Account name 2> - transfer amount from Account 1 to Account 2");
+					
 					while(true) {
 						String request = in.readLine();
 						System.out.println("Request from " + customer.getKey());
-						String responce = bank.processRequest(customer, request);
-						out.println(responce);
+						String response = bank.processRequest(customer, request);
+						out.println(response);
 					}
 				}
 			}
@@ -103,6 +80,49 @@ public class NewBankClientHandler extends Thread{
 				Thread.currentThread().interrupt();
 			}
 		}
+	}
+
+	public String greetUser() throws IOException {
+		out.println(
+		"####################\n" +
+		"Welcome to New Bank\n" +
+		"####################");
+		out.println("Type LOGIN if you are an existing user, or REGISTER if you are a new user");
+		String intialCommand = in.readLine();
+		boolean requestValid = false;
+		while(!requestValid) {
+			if(intialCommand.equals("LOGIN") || intialCommand.equals("REGISTER")) {
+				requestValid = true;
+			}
+			else {
+				out.println("Command not recognised. Please enter command LOGIN or REGISTER");
+				intialCommand = in.readLine();
+			}
+		}
+		return intialCommand;
+	}
+
+	public CustomerID login() throws IOException {
+		CustomerID customer = null;
+		while (true) {
+			// ask for user name
+			out.println("Enter Username");
+			String userName = in.readLine();
+			// ask for password
+			out.println("Enter Password");
+			String password = in.readLine();
+			out.println("Checking Details...");
+			customer = bank.checkLogInDetails(userName, password);
+			//Validate login details
+			if(customer == null) {
+				out.println("Log In Failed. Invalid Credentials, please try again.");
+			}
+			else {
+				out.println("Log In Successful. What do you want to do?");
+			}
+			return customer;
+		}
+		
 	}
 
 }
