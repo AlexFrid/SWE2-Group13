a58c326784a33fb31f6b8b2e6e902269ed99ded0
nat-bialek <93878157+nat-bialek@users.noreply.github.com>
2021-11-28 20:54:28 +0000

    cleaned up Account.java file

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -3,14 +3,12 @@ package newbank.server;
 public class Account {
 	
 	private String accountName;
-	//private double openingBalance;
 	private double currentBalance;
 
 	
 
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
-		//this.openingBalance = openingBalance;
 		this.currentBalance = openingBalance;
 	}
 
